1e57f56216848169621c2043b97322da1947606f
MikeEngerer <michael.engerer@hotmail.com>
2019-02-09 15:47:19 -0500

    format output

--- index.js
+++ index.js
@@ -19,27 +19,22 @@ operations.multiply = function() {
 
 operations.changeOp = function(type) {
 	this.currentOp = type
+	document.getElementById('current-op').innerHTML = this.currentOp
 }
 
 operations.changeNum = function(num) {
 	!this.numOne ? this.numOne = parseInt(num) : this.numTwo = parseInt(num)
+	this.displayNums()
 	return console.log(this)
 }
 
-operations.evaluate = function() {
-	if (this.hasInput()) {
-		return
-	}
-	let output = document.getElementById('output')
-	output.innerHTML = this[this.currentOp]();
-	this.numOne = this[this.currentOp]();
-}
-
 operations.displayNums = function() {
-	let one = document.getElementById('current-one')
-	let two = document.getElementById('current-two')
-	one.innerHTML = this.numOne;
-	two.innerHTML = this.numTwo;
+	document.getElementById('current-one').innerHTML = this.numOne;
+	if (this.numTwo) {
+		document.getElementById('current-two').innerHTML = this.numTwo;
+		document.getElementById('equals').innerHTML = '='
+		this.evaluate()
+	}
 }
 // operations.clearNums = function() {
 // 	this.
@@ -50,3 +45,19 @@ operations.hasInput = function() {
 		return true;
 	}
 }
+
+operations.hasOp = function() {
+	if (!this.currentOp) {
+		return true;
+	}
+	return false
+}
+operations.evaluate = function() {
+	if (this.hasInput() && this.hasOp()) {
+		return
+	}
+	let output = document.getElementById('output')
+	output.innerHTML = this[this.currentOp]();
+	this.numOne = this[this.currentOp]();
+}
+
